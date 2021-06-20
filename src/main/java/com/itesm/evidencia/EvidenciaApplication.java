df3628c75914ee0af23095602e0b1346546e1222
Braulio Bracamonte <35633915+BraulioBracamonte@users.noreply.github.com>
2021-06-20 12:26:11 -0500

    Add files via upload

---
+++ src/main/java/com/itesm/evidencia/EvidenciaApplication.java
@@ -0,0 +1,13 @@
+package com.itesm.evidencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvidenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvidenciaApplication.class, args);
+	}
+
+}
